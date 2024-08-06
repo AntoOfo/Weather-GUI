a15c9b0904060aca1abdef107df67ef67d5e4fde
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-06 23:51:53 +0100

    Menu UI sorted

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import font
 from PIL import Image, ImageTk
 import requests
 from datetime import datetime
@@ -13,21 +14,42 @@ def menu():
     
     icon = PhotoImage(file="sunicon.png")
     menu_window.iconphoto(True, icon) 
+    menu_window.config(background="#101010")
     
-    menu_bg = Image.open("menugradient.png")
-    menu_bg_resized = menu_bg.resize((390,600), Image.LANCZOS)
-    menu_bg_photo = ImageTk.PhotoImage(menu_bg_resized)
-    
-    background = Label(menu_window,
-                  image=menu_bg_photo)
-    background.place(x=0,y=0)
-    
+    main_font = font.Font(family="Aileron Bold", size=35)
+    secondary_font = font.Font(family="Aileron Light", size=10)
     header = Label(menu_window,
                    text="Weatherly",
-                   font=("Trebuchet MS",35),
+                   font=(main_font),
                    fg="White",
-                   bg=None)
-    header.place(x=80, y=50)
+                   bg="#101010")
+    header.place(x=75, y=50)
+    
+    desc = Label(menu_window,
+                 text="Your Personalised Weather Companion",
+                 font=(secondary_font),
+                 fg="Grey",
+                 bg="#101010")
+    desc.place(x=77, y=140)
+    
+    city_entry = Entry(menu_window)
+    city_entry.config(width=35)
+    city_entry.place(x=83, y=280 )
+    
+    entry_desc = Label(menu_window,
+                       text="City:",
+                       font=(secondary_font),
+                       fg="Grey",
+                       bg="#101010")
+    entry_desc.place(x=80, y=255)
+    
+    submitcity = Button(menu_window,
+                        text=">",
+                        font=(main_font,25),
+                        fg="White",
+                        bg="#101010",
+                        relief=FLAT)
+    submitcity.place(x=150,y=360)
     
     
     menu_window.mainloop()
